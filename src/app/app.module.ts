fc7a224b03dd36cde5c328051fa91813963f1a3c
Arne Hendrickx <arne.hendrickx.05@gmail.com>
2018-09-20 10:35:49 +0200

    Config for MsalModule changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './/app-routing.module';
+import {HomeComponent} from './home/home.component';
 import {MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule, MatGridListModule} from '@angular/material';
-import { GameComponent } from './game/game.component';
-import { TopscoresComponent } from './topscores/topscores.component';
+import {GameComponent} from './game/game.component';
+import {TopscoresComponent} from './topscores/topscores.component';
 import {MsalModule} from '@azure/msal-angular';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    GameComponent,
-    TopscoresComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-    MatGridListModule,
-    MsalModule.forRoot({
-        clientID: 'f993cf0d-9e6d-44b1-80f3-cf760d018618'
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        GameComponent,
+        TopscoresComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
+        MatGridListModule,
+        MsalModule.forRoot({
+            clientID: 'f993cf0d-9e6d-44b1-80f3-cf760d018618',
+            authority: 'https://login.microsoftonline.com/common/',
+            validateAuthority: true,
+            consentScopes: ['user.read'],
+            navigateToLoginRequestUrl: true,
+            popUp: false,
+            piiLoggingEnabled: true
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
