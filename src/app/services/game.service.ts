68c962a9c695112389ad5b0b4de2c45e64f212cf
Zento De Houwer <zentodehouwer@gmail.com>
2018-09-28 09:05:43 +0200

    fix endgame

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Player } from '../models/player.model';
+import { Game } from '../models/game.model';
 
 
 @Injectable({
@@ -14,5 +15,9 @@ export class GameService {
 
   startGame(player:Player){
     return this.http.post(this.apiURL + "/create", player);
+  }
+
+  postScore(game:Game){
+    return this.http.post(this.apiURL, game);
   }
 }
